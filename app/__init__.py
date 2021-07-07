8549c6cd2c7fee4acff28c62fbeac47af0b7d610
Ariana Rice <a_rice@live.ca>
2021-07-07 15:39:43 -0500

    Edit login and register endpoint code

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import json
 # from . import db
 from werkzeug.security import check_password_hash, generate_password_hash
-from app.db import get_db
+# from app.db import get_db
 # from flask.typing import StatusCode
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -53,56 +53,42 @@ def health():
     except (Exception) as e:
         return f"An error ocurre: {e}"
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
-
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
-
     ## TODO: Return a restister page
     return render_template('register.html', url=os.getenv("URL"))
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
+        user = UserModel.query.filter_by(username=username).first()
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
-
         if error is None:
-            return "Login Successful", 200 
+            return "Login Successful", 200
         else:
             return error, 418
-    
     ## TODO: Return a login page
     return render_template('login.html', url=os.getenv("URL"))
